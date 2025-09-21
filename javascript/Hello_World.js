db45ec6ba4a13ad6facceabf44a478cd2df8baee
Gary <gjcairns1@gmail.com>
2025-09-21 16:17:20 +0100

    Added new file with breakpoints

---
+++ javascript/Hello_World.js
@@ -0,0 +1,9 @@
+var greeting = "Hello, World!";
+console.log(greeting);
+
+var greeting2 = "Welcome to node.js!";
+console.log(greeting2);
+
+var sentence1 = "Learning how to debug code with a debugger";
+console.log(sentence1);
+
